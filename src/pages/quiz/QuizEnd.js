a8a79729e44db114947720ef805a32146fd7c652
Mahi Shah <mahishah@gmail.com>
2024-01-31 08:26:15 +0530

    removed warning

--- src/pages/quiz/QuizEnd.js
+++ src/pages/quiz/QuizEnd.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
